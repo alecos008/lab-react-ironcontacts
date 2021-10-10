86a8736aa5ebc8ccd98e1fb55e741fe9cae005fd
Alejandro Cazorla <acazorla72@gmail.com>
2021-10-10 17:25:43 +0200

    completed 4 iterations

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,14 +3,56 @@ import contacts from "../contacts.json";
 
 const contactCardStyle = {
   display: "flex",
-  columnGap: "10px",
+  columnGap: "50px",
 };
 
 const initialContacts = contacts.slice(0, 5);
 
 class Contacts extends Component {
+  //* Defining State
   state = {
     contactsList: initialContacts,
+  };
+
+  //* Method to generate random contact
+  handleAddContact = () => {
+    const { contactsList } = this.state;
+    let randContact = contacts[Math.floor(Math.random() * contacts.length)];
+    this.setState({ contactsList: [randContact, ...contactsList] });
+  };
+
+  handleSortByName = () => {
+    const { contactsList } = this.state;
+
+    let clonedList = [...contactsList];
+
+    clonedList.sort((a, b) => {
+      return a.name > b.name ? 1 : -1;
+    });
+
+    this.setState({ contactsList: clonedList });
+  };
+
+  handleSortByPop = () => {
+    const { contactsList } = this.state;
+
+    let clonedList = [...contactsList];
+
+    clonedList.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+
+    this.setState({ contactsList: clonedList });
+  };
+
+  handleRemove = (contactId) => {
+    const { contactsList } = this.state;
+
+    const filteredContacts = contactsList.filter((contact) => {
+      return contact.id != contactId ? true : false;
+    });
+
+    this.setState({ contactsList: filteredContacts });
   };
 
   render() {
@@ -22,18 +64,41 @@ class Contacts extends Component {
           flexDirection: "column",
         }}
       >
+        <div>
+          <h2>Generate Random Contact</h2>
+          <button onClick={this.handleAddContact}>Add Random Contact</button>
+          <button onClick={this.handleSortByName}>Sort by name</button>
+          <button onClick={this.handleSortByPop}>Sort by popularity</button>
+        </div>
         <h1>Contact List</h1>
-        {this.state.contactsList.map((contact) => (
-          <div style={contactCardStyle}>
-            <img
-              src={contact.pictureUrl}
-              alt="Contact Picture"
-              style={{ width: "100px", height: "150px" }}
-            />
-            <h2>{contact.name}</h2>
-            <h2>{contact.popularity}</h2>
-          </div>
-        ))}
+        <table>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Action</th>
+
+          {this.state.contactsList.map((contact) => (
+            <tr>
+              <td>
+                <img
+                  src={contact.pictureUrl}
+                  alt="Contact Picture"
+                  style={{ width: "100px", height: "150px" }}
+                />
+              </td>
+              <td>
+                <p>{contact.name}</p>
+              </td>
+              <td>
+                <p>{contact.popularity.toFixed(2)}</p>
+              </td>
+              <td>
+                <button onClick={() => this.handleRemove(contact.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </table>
       </div>
     );
   }
